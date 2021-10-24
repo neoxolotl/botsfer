7a58b53e9b5d8acaa76dab5385820ba8a634d900
fernandoum <fernanditososa@gmail.com>
2021-10-24 14:23:19 -0300

    color

--- scr-dol-cry/cotiz.sh
+++ scr-dol-cry/cotiz.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+PATH=/bin:/usr/bin:
+
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+echo -e "This text is ${RED}red${NONE} and ${GREEN}green${NONE} and ${BOLD}bold${NONE} and ${UNDERLINE}underlined${NONE}."
+
+
 while :
 do 
 	#curl https://api.bluelytics.com.ar/v2/latest > file
